5f963560d00de7aa6062e8654f6cad93008c37b2
Lucas Lopes <lucas.lrosa@ambevtech.com.br>
2023-09-09 16:53:04 -0300

    feat: adding assets and implementing routers and header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,15 @@
+import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
+import { ReactComponent as Logo } from '../../Assets/dogs.svg';
 
 export const Header = () => {
   return (
-    <div className={styles.header}>Header</div>
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to='/' aria-label='Pets - Home'><Logo/></Link>
+        <Link className={styles.loginButton} to='/login'>Login / Criar</Link>
+      </nav>
+    </header>
   )
 }
 
