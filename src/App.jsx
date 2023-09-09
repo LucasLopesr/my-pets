b5998074a5b3996d154665c8b0e12f9e7acead7b
Lucas Lopes <lucas.lrosa@ambevtech.com.br>
2023-09-09 16:16:12 -0300

    init project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <div>App component</div>
+      <Footer/>
+
+    </>
+  )
+}
+
+export default App
