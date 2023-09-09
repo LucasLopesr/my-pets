55a5ed6c46f35373df34f75a9805d83e95f5aefe
Lucas Lopes <lucas.lrosa@ambevtech.com.br>
2023-09-09 17:53:42 -0300

    refactor: changing pages structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
-import Home from './Components/Home/Home';
-import Login from './Components/Login/Login';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
 
 function App() {
 
