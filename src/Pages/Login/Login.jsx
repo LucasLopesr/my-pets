80953e1d5a9a0d1ed560e4fb1c58c85f87f0716d
Lucas Lopes <lucas.lrosa@ambevtech.com.br>
2023-09-17 18:28:56 -0300

    feat: styling login page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,10 +1,11 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { useContext } from 'react';
 import LoginUser from './LoginUser/LoginUser';
 import CreateUser from './CreateUser/CreateUser';
 import RecoverPass from './RecoverPass/RecoverPass';
 import ResetPass from './ResetPass/ResetPass';
 import { UserContext } from '../../Contexts/UserContext';
-import { useContext } from 'react';
+import styles from './Login.module.css';
 
 const Login = () => {
   const { isUserLogged } = useContext(UserContext);
@@ -14,14 +15,16 @@ const Login = () => {
   }
 
   return (
-    <div>
-      <Routes>
-        <Route path='/' element={<LoginUser/>}/>
-        <Route path='create' element={<CreateUser/>}/>
-        <Route path='recover' element={<RecoverPass/>}/>
-        <Route path='reset-pass' element={<ResetPass/>}/>
-      </Routes>
-    </div>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path='/' element={<LoginUser/>}/>
+          <Route path='create' element={<CreateUser/>}/>
+          <Route path='recover' element={<RecoverPass/>}/>
+          <Route path='reset-pass' element={<ResetPass/>}/>
+        </Routes>
+      </div>
+    </section>
   )
 }
 
